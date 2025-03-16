a17ee7af0e836a2dc9f5837d3deceaf32a7598e4
Chen Wei Lun <weilunchen210@gmail.com>
2025-03-16 19:43:47 +0800

    initial commit

---
+++ src/main/java/com/UniEnroll/UniEnroll/UniEnrollApplication.java
@@ -0,0 +1,13 @@
+package com.UniEnroll.UniEnroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniEnrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniEnrollApplication.class, args);
+	}
+
+}
